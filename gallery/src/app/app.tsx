8ab9183fd2d2bfef7117a19fcbb74bd05af357ce
sdrahichynski <siarhei.drahichynski@gmail.com>
2023-04-08 11:55:10 +0300

    ci: initial

---
+++ gallery/src/app/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface AppProps {
+};
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <div>
+      HALLO!
+    </div>
+  );
+};
+
+export default App;
