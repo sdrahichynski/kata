780d3a656b81b03853ffafbb7d0ecc06599f4b5d
sdrahichynski <siarhei.drahichynski@gmail.com>
2023-05-17 12:32:26 +0300

    feat: initial

--- todo-board/src/app/app.tsx
+++ todo-board/src/app/app.tsx
@@ -1,14 +1,7 @@
-import * as React from "react";
 import * as C from "components";
 
-interface AppProps {}
-
-const App: React.FC<AppProps> = () => {
-  return (
-    <div>
-      <C.Board />
-    </div>
-  );
+const App = () => {
+  return <C.Board />;
 };
 
 export default App;
