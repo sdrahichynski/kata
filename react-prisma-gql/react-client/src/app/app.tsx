6026fcc1048bcd87dbffb0db52eb220bcebbcda7
sdrahichynski <siarhei.drahichynski@gmail.com>
2023-07-15 22:28:49 +0300

    feat: init client gql

--- react-prisma-gql/react-client/src/app/app.tsx
+++ react-prisma-gql/react-client/src/app/app.tsx
@@ -1,24 +1,18 @@
 import * as C from "../components";
-import * as T from "types";
-
-const USERS_MOCK: T.User[] = [
-  {
-    name: "Prisma Fan",
-    messages: [
-      {
-        body: "Prisma rocks!!",
-      },
-      {
-        body: "Did I mention I love Prisma?",
-      },
-    ],
-  },
-];
+import { useQuery } from "urql";
+import { GetUsersDocument } from "../graphql/generated";
 
 function App() {
+  const [result] = useQuery({
+    query: GetUsersDocument,
+    variables: {},
+  });
+
+  console.log(result.data);
+
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p4 gap-y-12">
-      {USERS_MOCK.map((user, i) => (
+      {result.data?.users.map((user, i) => (
         <C.UsersDisplay user={user} key={i} />
       ))}
     </div>
--- react-prisma-gql/react-client/src/main.tsx
+++ react-prisma-gql/react-client/src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 import "./index.css";
+import { Provider, createClient, fetchExchange } from "urql";
+
+const client = createClient({
+  exchanges: [fetchExchange],
+  url: "http://localhost:4000/graphql",
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider value={client}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
