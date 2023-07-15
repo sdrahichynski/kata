10b1c67dfb447617e0f33b1c7828cfb37078bca8
sdrahichynski <siarhei.drahichynski@gmail.com>
2023-07-15 08:19:21 +0300

    feat: init tailwind

---
+++ react-prisma-gql/react-client/src/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p4 gap-y-12">
+      <h2 className="text-4xl text-yellow-500">HELLO!</h2>
+    </div>
+  );
+}
+
+export default App;
